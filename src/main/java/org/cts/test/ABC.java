3a5221ab8a8ff2187880f59c63f31022d6a05933
jayasankar2020 <sjayasankar96@gmail.com>
2020-02-24 15:23:53 -0800

    i have done my system

--- src/main/java/org/cts/test/ABC.java
+++ src/main/java/org/cts/test/ABC.java
@@ -1,5 +1,8 @@
 package org.cts.test;
 
 public class ABC {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ADMIN\\eclipse-jayasankar\\Selenium\\drivers\\chromedriver.exe");
+		}
 
 }
